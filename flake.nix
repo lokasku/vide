6035a61b10cd0010cd2a5bf792ecf15d8bf96ad5
Bartek <lukasku@proton.me>
2025-03-13 19:41:29 +0100

    Add aarch64-darwin arch

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     kks-source,
     ...
   }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin"];
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f:
       nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
 
